c5ff91b08395fd4a0171d3a7568e9cc5b5963643
Karthikpillai77 <56841541+Karthikpillai77@users.noreply.github.com>
2022-10-17 12:45:30 +0530

    Create celsiustofahrenheit.js

---
+++ celsiustofahrenheit.js
@@ -0,0 +1,6 @@
+
+const celsius = prompt("Enter a celsius value: ");
+
+const fahrenheit = (celsius * 1.8) + 32
+
+console.log(`${celsius} degree celsius is equal to ${fahrenheit} degree fahrenheit.`);
